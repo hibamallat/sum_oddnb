beb8bd0e694747fab4f7259b651881ea75035365
hibamallat <hibamallat040@gmail.com>
2024-10-26 18:48:49 +0300

    add the comments line

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
+#The user must enter a number n
 n = int(input("Enter n please: "))
+#At the start the sum is empty 
 sum = 0
+#If the number is odd, add it to the sum
 for i in range (n+1):
     if i % 2 != 0 :
         sum += i
