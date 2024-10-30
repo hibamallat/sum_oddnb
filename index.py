0577d63ce7501bc624c9542b9136f0265b80e223
hibamallat <hibamallat040@gmail.com>
2024-10-30 17:23:51 +0200

    file updated

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 #The user must enter a number n
 n = int(input("Enter n please: "))
-#At the start the sum is empty 
+#At the beginning, the sum is 0 
 sum = 0
-#If the number is odd, add it to the sum
+
 for i in range (n+1):
-    if i % 2 != 0 :
+    if i % 2 != 0 : #If the number is odd, add it to the sum
         sum += i
 
 print("The sum of the odd numbers is ", sum)
